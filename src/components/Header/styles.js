b71128c7134152d5f3a1ffdff5884ae02816fa31
mandala21 <lucasresone@gmail.com>
2019-08-03 10:44:52 -0300

    header ok

---
+++ src/components/Header/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default styled.div`
+    display:flex;
+    background: #8151c1;
+    color: #fff;
+    align-items: center;
+    height: 80px;
+    padding: 0 30px;
+`;
