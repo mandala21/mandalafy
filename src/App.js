a6f2329d5eee0588783f1dc61e05f5873865ad97
mandala21 <lucasresone@gmail.com>
2019-08-03 11:35:17 -0300

    inicio dos list e cards

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import GlobalStyles from './styles/global';
 import Header from "./components/Header";
+import Board from "./components/Board";
 
 function App() {
   return (
     <>
       <Header />
-      
+      <Board />
+
       <GlobalStyles />
     </>
   );
